731458fac755482b2ffeae00aaadcec72833473b
JimyTD <604384365@qq.com>
2023-02-23 21:52:22 +0800

    增加房间UI、travel逻辑

---
+++ RoadMaster2/Source/RoadMaster2/GameState/OldVersionRMGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InBattleGameState.h"
+#include "OldVersionRMGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROADMASTER2_API AOldVersionRMGameState : public AInBattleGameState
+{
+	GENERATED_BODY()
+	
+};
