2e20bade092cd02b7afdabab17cab9505d4212ba
JimyTD <604384365@qq.com>
2023-03-02 22:01:55 +0800

    不完整的信息

--- RoadMaster2/Source/RoadMaster2/GameMode/SimpleFightGameMode.h
+++ RoadMaster2/Source/RoadMaster2/GameMode/SimpleFightGameMode.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
 #include "GameFramework/GameModeBase.h"
 #include "SimpleFightGameMode.generated.h"
 
@@ -10,8 +11,46 @@
  * 
  */
 UCLASS()
-class ROADMASTER2_API ASimpleFightGameMode : public AGameModeBase
+class ROADMASTER2_API ASimpleFightGameMode : public AGameMode
 {
 	GENERATED_BODY()
+	#pragma region    >>> Override Functions
+public:
+	virtual void PostInitializeComponents() override;
 	
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	virtual APlayerController* PreCreatePlayerController(uint32 PlayerKey);
+
+	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, const FString& Options) override;
+
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+	
+	virtual APlayerController* Login(class UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	
+	virtual void Logout(AController* Exiting) override;
+
+	virtual void RestartPlayerAtPlayerStart(AController* NewPlayer, AActor* StartSpot) override;
+
+	virtual void RestartPlayerAtTransform(AController* NewPlayer, const FTransform& SpawnTransform);
+
+	virtual void FinishRestartPlayer(AController* NewPlayer, const FRotator& StartRotation) override;
+
+	virtual bool AllowCheats(APlayerController* P) override;
+
+	virtual void UpdateGameplayMuteList(APlayerController* aPlayer) override;
+
+	virtual bool MustSpectate_Implementation(APlayerController* NewPlayerController) const override;
+
+	uint32 GenerateUniqueKeyEditor();
+	
+	TSet<uint32> EditorPlayerKeySet;
+
+#pragma endregion <<< Override Functions 
+
+
 };
